e976141df65b6015d51ba1314f3eadae161f869d
zhongxuqi <zhongxuqi@foxmail.com>
2021-07-09 23:32:57 +0800

    init app

---
+++ app/android/app/src/main/kotlin/com/musketeer/ipfspass/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.musketeer.ipfspass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- backend/scripts/curl.sh
+++ backend/scripts/curl.sh
@@ -5,7 +5,6 @@ curl -X POST "http://127.0.0.1:9990/api/content" -H "Content-Type: application/j
 
 curl "http://127.0.0.1:8080/ipfs/QmZpcs8ga3ZsGsj5xVy7V3mXQ121TpvQQvSsE7A8W9PtQV"
 
-
 curl -X POST -F file=@ipfspass.txt "http://127.0.0.1:6001/api/v0/add?hash=sha2-256&inline-limit=32"
 curl -X POST -F file=@ipfspass.txt "https://3cloud.ee:5001/api/v0/add?hash=sha2-256&inline-limit=32"
 curl -X POST -F file=@ipfspass.txt "https://ipfs.mihir.ch:5001/api/v0/add?hash=sha2-256&inline-limit=32"

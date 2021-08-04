c9ad6afd82428aa17bad0a98ae3fa841feac0b0d
zhongxuqi <zhongxuqi@foxmail.com>
2021-08-05 00:13:20 +0800

    modify uploadIPFS

--- app/lib/utils/ipfs.dart
+++ app/lib/utils/ipfs.dart
@@ -2,16 +2,10 @@ import 'package:dio/dio.dart';
 
 class IPFSUtils {
   static Future<Response> uploadIPFS(String data) async {
-    // return http.post(Uri.parse('https://infura-ipfs.io:5001/api/v0/add?hash=sha2-256&inline-limit=32'), headers: {
-    //   'Content-Type': 'multipart/form-data',
-    // }, body: {
-    //   "file": data,
-    // });
-
     var dio = Dio();
     var formData = FormData.fromMap({
       'file': MultipartFile.fromString(data),
     });
-    return dio.post('https://infura-ipfs.io:5001/api/v0/add?hash=sha2-256&inline-limit=32', data: formData);
+    return dio.post('https://infura-ipfs.io:5001/api/v0/add?hash=sha2-256&inline-limit=32', data: formData).timeout(Duration(seconds: 5));
   }
 }

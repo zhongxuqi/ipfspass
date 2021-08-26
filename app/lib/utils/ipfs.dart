b77e32bfa9c07760d53249ae0e4d6d856c27fd66
zhongxuqi <zhongxuqi@foxmail.com>
2021-08-27 00:00:22 +0800

    add init gateway

--- app/lib/utils/ipfs.dart
+++ app/lib/utils/ipfs.dart
@@ -1,24 +1,35 @@
 import 'package:dio/dio.dart';
 
 class IPFSUtils {
+  static var dio = Dio();
+  static String readGateway = "";
+  static String writeGateway = "";
+
+  static void initGateway() async {
+    if (readGateway.isNotEmpty && writeGateway.isNotEmpty) return;
+    var response = await dio.get('https://ipfs.easypass.tech/api/ipfs').timeout(Duration(seconds: 5));
+    if (response.data["errno"] != 0) {
+      throw "init gateway final";
+    }
+    readGateway = response.data["data"]["read_gateway"][0];
+    writeGateway = response.data["data"]["write_gateway"][0];
+  }
+
   static Future<Response> uploadIPFS(String data) async {
-    // todo 获取gateway
-    var dio = Dio();
+    await initGateway();
     var formData = FormData.fromMap({
       'file': MultipartFile.fromString(data),
     });
-    return dio.post('https://infura-ipfs.io:5001/api/v0/add?hash=sha2-256&inline-limit=32', data: formData).timeout(Duration(seconds: 5));
+    return dio.post('$writeGateway/api/v0/add?hash=sha2-256&inline-limit=32', data: formData).timeout(Duration(seconds: 5));
   }
 
   static Future<Response> convertIPFSContentID(String contentID) async {
-    var dio = Dio();
     return dio.get('https://ipfs.easypass.tech/api/ipfs/cid/base32', queryParameters: {'arg': contentID}).timeout(Duration(seconds: 5));
   }
 
   static Future<Response> downloadFromIPFS(String contentID) async {
-    // todo 获取gateway
-    var url = "https://<cidv1>.ipfs.infura-ipfs.io/".replaceAll("<cidv1>", contentID);
-    var dio = Dio();
+    await initGateway();
+    var url = readGateway.replaceAll("<cidv1>", contentID);
     return dio.get(url).timeout(Duration(seconds: 10));
   }
 }
